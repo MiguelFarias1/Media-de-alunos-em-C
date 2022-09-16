0ce61cc56e093752c35a663e5d2d3e7e6d08c409
Miguel Farias <miguelfarias@alu.ufc.br>
2022-09-16 10:17:04 -0300

    Programa atualizado com função que verifica se as notas são válidas.

---
+++ Biblioteca.h
@@ -0,0 +1,97 @@
+//
+// Created by Miguel on 16/09/2022.
+//
+#ifndef MEDIA_BIBLIOTECA_H
+#define MEDIA_BIBLIOTECA_H
+#include<locale.h>
+
+struct Aluno
+{
+    char nome[100];
+    int matricula;
+    float nota1,nota2,nota3;
+};
+typedef struct Aluno Aluno;
+
+void VerificaNota(Aluno *aluno,int quantidade);
+
+void Preenche(Aluno *aluno, int quantidade)
+{
+    setlocale(LC_ALL,"portuguese");
+
+    for (int i = 0; i < quantidade; i++)
+    {
+        fflush(stdin);
+
+        printf("Digite o nome do aluno %d !\n", i + 1);
+        gets(aluno[i].nome);
+
+        fflush(stdin); // Comando necessário para limpar o buffer do teclado !
+
+        printf("Digite a matrícula do aluno !\n");
+        scanf("%d", &aluno[i].matricula);
+
+        fflush(stdin);
+
+        printf("Digite a nota 1 do nota do aluno: %s!\n", aluno[i].nome);
+        scanf("%f", &aluno[i].nota1);
+
+        VerificaNota(&aluno[i],i);
+
+        fflush(stdin);
+
+        printf("Digite a nota 2 do nota do aluno: %s !\n", aluno[i].nome);
+        scanf("%f", &aluno[i].nota2);
+
+        VerificaNota(&aluno[i],i);
+
+        fflush(stdin);
+
+        printf("Digite a nota 3 do nota do aluno: %s !\n", aluno[i].nome);
+        scanf("%f", &aluno[i].nota3);
+
+        VerificaNota(&aluno[i],i);
+    }
+}
+
+void VerificaNota(Aluno *aluno, int i)
+{
+        if(aluno[i].nota1 > 10 || aluno[i].nota1<0)
+            while(aluno[i].nota1 > 10 || aluno[i].nota1<0)
+            {
+                fflush(stdin);
+
+                printf("Você tem que digitar uma nota entre 0 e 10 !\n");
+                printf("Digite a nota 1 do nota do aluno: %s!\n", aluno[i].nome);
+                scanf("%f", &aluno[i].nota1);
+            }
+
+        else if(aluno[i].nota2 > 10 || aluno[i].nota2<0)
+            while(aluno[i].nota2 > 10 || aluno[i].nota2<0)
+            {
+                fflush(stdin);
+
+                printf("Você tem que digitar uma nota entre 0 e 10 !\n");
+                printf("Digite a nota 2 do nota do aluno: %s!\n", aluno[i].nome);
+                scanf("%f", &aluno[i].nota2);
+            }
+
+        else if(aluno[i].nota3 > 10 || aluno[i].nota3<0)
+            while(aluno[i].nota3 > 10 || aluno[i].nota3<0)
+            {
+                fflush(stdin);
+
+                printf("Você tem que digitar uma nota entre 0 e 10 !\n");
+                printf("Digite a nota 3 do nota do aluno: %s!\n", aluno[i].nome);
+                scanf("%f", &aluno[i].nota3);
+            }
+}
+
+float CalculaMedia(Aluno * aluno, int i)
+{
+    float soma = aluno[i].nota1 + aluno[i].nota2 + aluno[i].nota3;
+
+    return soma / 3 ;
+}
+
+#endif //MEDIA_BIBLIOTECA_H
--- main.c
+++ main.c
@@ -1,18 +1,9 @@
 #include <stdio.h>
 #include<stdlib.h>
-#include<locale.h>
-
-struct Aluno
-{
-    char nome[100];
-    int matricula;
-    float nota1,nota2,nota3;
-};
-typedef struct Aluno Aluno;
+#include "Biblioteca.h"
 
 int main()
 {
-
     setlocale(LC_ALL,"portuguese");
 
     int quantidade = 0;
@@ -20,7 +11,7 @@ int main()
     printf("Digite a quantidade de alunos da turma !\n");
     scanf("%d",&quantidade);
 
-    while(quantidade < 0)   // While para garantir que vai ser um número positivo !
+    while(quantidade < 0)   // While para garantir que vai ser um número positivo!
     {
         printf("Digite a quantidade de alunos da turma !\n");
         scanf("%d",&quantidade);
@@ -31,47 +22,19 @@ int main()
 
     Aluno * aluno = (Aluno *) calloc(quantidade,sizeof (Aluno));
 
-    for(int i=0; i<quantidade; i++)
-    {
-        fflush(stdin);
-
-        printf("Digite o nome do aluno %d !\n",i+1);
-        gets(aluno[i].nome);
-
-        fflush(stdin); // Comando necessário para limpar o buffer do teclado !
-
-        printf("Digite a matrícula do aluno !\n");
-        scanf("%d",&aluno[i].matricula);
-
-        fflush(stdin);
-
-        printf("Digite a nota 1 do nota do aluno: %s!\n",aluno[i].nome);
-        scanf("%f",&aluno[i].nota1);
-
-        fflush(stdin);
-
-        printf("Digite a nota 2 do nota do aluno: %s !\n",aluno[i].nome);
-        scanf("%f",&aluno[i].nota2);
-
-        fflush(stdin);
-
-        printf("Digite a nota 3 do nota do aluno: %s !\n",aluno[i].nome);
-        scanf("%f",&aluno[i].nota3);
-    }
-
-    float media = 0;
+    Preenche(aluno,quantidade);
 
     for(int i=0; i<quantidade;i++)
     {
-        media = aluno[i].nota1 + aluno[i].nota2 + aluno[i].nota3;
-        media /= 3;
+        float media = CalculaMedia(&aluno[i], i);
+        // Armazenei o retorno da função em uma variável para o código ficar mais legível.
 
-        if(media >=7)
-            printf("O aluno %s está aprovado !\n",aluno[i].nome);
-        else if(media <7 && media >=4)
-            printf("O aluno %s está de avaliação final !\n",aluno[i].nome);
-        else if(media < 4)
-            printf("O aluno %s está eprovado !",aluno[i].nome);
+        if ( media >= 7)
+            printf("O aluno %s tem a média %.2f e está aprovado !", aluno[i].nome ,media);
+        else if(media < 7 && media >=4)
+            printf("O aluno %s tem a média %.2f e está de avaliação final !",aluno[i].nome , media);
+        else if(media<4)
+            printf("O aluno %s tem a media %.2f e está reprovado !",aluno[i].nome,media);
     }
     return 0;
 }
